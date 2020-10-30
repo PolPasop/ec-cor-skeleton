06e490c0cb694511f54b12fa1f3359a88e31c992
Jesse Govart <govartjessy@gmail.com>
2020-10-30 18:14:47 +0100

    speakers pictures

--- components/europcom/speakers.config.js
+++ components/europcom/speakers.config.js
@@ -1,11 +1,12 @@
 const faker = require('faker');
+
 const speakersCount = 32;
 const speakersData = [];
 
 for (var i = 0; i < speakersCount; i++) {
   speakersData.push({
     name: faker.name.findName(),
-    picture: faker.image.nature(),
+    picture: "https://fakeface.rest/face/view/" + i + "?minimum_age=25",
     jobTitle: faker.name.jobTitle(),
     company: faker.company.companyName(),
     bio: faker.lorem.paragraphs(),
