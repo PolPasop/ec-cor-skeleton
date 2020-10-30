a56739cd666d029f22a02fd32679ebd701b10673
Jesse Govart <govartjessy@gmail.com>
2020-10-30 13:51:20 +0100

    speakers

--- components/europcom/speakers.config.js
+++ components/europcom/speakers.config.js
@@ -7,11 +7,13 @@ for (var i = 0; i < speakersCount; i++) {
     name: faker.name.findName(),
     picture: faker.image.nature(),
     jobTitle: faker.name.jobTitle(),
-    company: faker.company.companyName()
+    company: faker.company.companyName(),
+    bio: faker.lorem.paragraphs()
   });
 }
 
 module.exports = {
+  preview: "@europcompreview",
   context: {
     speakers: speakersData
   }
