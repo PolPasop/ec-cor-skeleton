43d6f0816461c97b12a85aa04b5be5215e09f629
Jesse Govart <govartjessy@gmail.com>
2020-11-03 14:09:50 +0100

    program

---
+++ components/europcom/program.config.js
@@ -0,0 +1,197 @@
+const data7 = [
+  {
+    "date": "07.10.20",
+    "time": "09:45 - 10:30",
+    "title": "The EuroPCom Morning Show",
+    "description": "Grab a coffee and kick-start your neurons with this informal introduction to EuroPCom 2020!"
+  }, {
+    "date": "07.10.20",
+    "time": "10:30 - 12:00",
+    "title": "Opening session -  Time for Communica[c]tion",
+    "description": "A high-level panel of experts in the three conference themes of Citizens, Green and Digital, share their thoughts and ideas on communicating in the current pandemic and on the two flagship initiatives, and answer your questions.",
+    "more": "<p>The COVID-19 pandemic has clearly highlighted the need for communication lead to action, as well as the key role of communication in facilitating government action and public response.</p><p>In this opening session of EuroPCom 2020 – Time for Communica[c]tion, the panel of expert speakers, drawn from organisations active in each of the three conference themes, will share their thoughts and ideas on communicating in the current pandemic, and the two flagship initiatives.</p>"
+  },
+  {
+    "date": "07.10.20",
+    "time": "12:15 - 13:15",
+    "title": "EuroPCom Talks ",
+    "description": "Three inspiring speakers, each an expert in one of the conference themes of Citizens, Green and Digital, will share their views on communicating around their area of expertise."
+  },
+  {
+    "date": "07.10.20",
+    "time": "13:15 - 13:20",
+    "title": "Tell it like it is! ",
+    "description": "Light-hearted review of the morning session."
+  },
+  {
+    "date": "07.10.20",
+    "time": "13:20 - 14:15",
+    "title": "Eat and meet in the EuroPCom Market Place! ",
+    "description": "Time to relax, grab some lunch, and meet old and new friends in the EuroPCom 2020 Market Place. Plus chat with the many different projects and organisations which will be exhibiting there. ",
+    "more": "Our Virtual Marketplace will host multiple exhibitors who are eager to present their projects and exchange ideas with you. Please stroll around the virtual booths, make contact with the different organisations and projects, schedule 1:1 meetings, or follow group demo sessions. The Market Place will be open from lunchtime to 18:00 on day 1, and from 09:00 until 17:00 on day 2."
+  },
+  {
+    "date": "07.10.20",
+    "time": "14:15 - 15:45",
+    "type": "Workshops",
+    "title": "Workshops",
+    "description": "Follow the exchange between the panelists and seize the chance to fire your questions at them.",
+    "workshops": [
+      {
+        "title": "The Conference on the Future of Europe: How to reflect the new reality?",
+        "category": "Citizens",
+        "more": "Announced at the end of 2019, the Conference on the Future of Europe aims to give all stakeholders, including citizens, a greater say on what the European Union does and how it works for them. The launch of the Conference was delayed due to the COVID-19 outbreak, as all attention was shifted towards dealing with the pandemic. Yet, democracy is stronger than a virus, and revamping European democracy remains a priority of the EU. So the big question now is how to adapt the Conference on the Future of Europe to the current situation? Find out directly from the organisers of the Conference what is envisaged, and share your views and expectations on how to successfully launch and carry out the process!"
+      },
+      {
+        "title": "#EUGreenDeal – What's in it for communication?",
+        "category": "Green",
+        "more": "In 2019, for the first time in a Eurobarometer survey, European citizens said that climate change should be the top priority for the EU institutions. The European Green Deal is the EU's response to tackle climate and environment related challenges and was launched only eleven days after the new European Commission took office. The plan comes with a major communication campaign. What are your expectations? How can we engage the public in ambitions for climate action? Does the COVID-19 outbreak change the game? Ask your questions and give your input!"
+      },
+      {
+        "title": "Crisis communication: managing the unmanageable",
+        "category": "Digital",
+        "more": "Whether it is a global pandemic or a local emergency, organisations and public authorities seem to face a constant pressure of crisis management. Effective handling of crisis communication among staff and towards the outside world rests in preparedness and anticipation. We may not be able to control the roots or consequences of the crisis, but we can control our response to it. What have we learned in the past months that will help us prepare for those emergencies that are yet to come? Join us in this important sharing and learning exercise!"
+      }
+    ]
+  },
+  {
+    "date": "07.10.20",
+    "time": "16:00 - 17:30",
+    "type": "Ideas Labs",
+    "title": "Ideas Labs – time to experiment",
+    "description": "Deep dive into your selected theme to actively work online with other participants! Get prepared to share your ideas and experiences with the other members of the Lab! ",
+    "more": '<p>The Ideas Labs will be very collaborative and participatory workshops, best suited for people who are willing to work together in small, facilitated groups to exchange their ideas and views with other participants. Each Ideas Lab will be different in terms of subject matter and what it expects to generate, from new ideas and concepts to concrete plans for future action.</p><p>Due to the participatory nature of the labs the number of participants in each Ideas Lab will be limited. Registrations for a particular Lab will be closed when sufficient people have indicated they wish to join it. Everyone who was able to register their interest will be allowed enter the "lobby" of their Ideas Lab shortly before it is due to begin, with access to the actual Lab granted on a first-come first-served basis.</p><p>If you do not wish to participate in an Ideas Lab, or the available spaces have all been taken up, we encourage you to visit the EuroPCom Market Place, meet other participants and find out more about the many projects and organisations who will be exhibiting.</p>',
+    "workshops": [
+      {
+        "title": "FuturGov engagement game",
+        "category": "Citizens",
+        "more": "The FuturGov serious game uses people’s anticipatory assumptions about what the future may look like to generate conversations, negotiations and collaborations. By designing a process through which participants immerse themselves into the future, take on roles that are not theirs, and strategise to achieve their goals, the game generates a participatory setting in which a debate on future of government models can take place."
+      },
+      {
+        "title": "Narratives for climate communication",
+        "category": "Green",
+        "more": "This Ideas Lab aims to equip delegates with tools and techniques to create persuasive messages [and frames] around the fight against climate change. The techniques to be introduced include collecting opposing messages and critical arguments, finding powerful replies, and reframing the original positive concepts."
+      },
+      {
+        "title": "Is the future of participatory democracy digital?",
+        "category": "Digital",
+        "more": "Now more than ever, citizens can participate in decision-making online. New hybrid platforms combine physical and distant consultation elements, and innovative digital forms of participatory democracy now often supplement representative democracy. The COVID-19 pandemic has made people reassess how they regard digital participation, and it seems unlikely that we will go back to how many regarded digital participation in the past. Join this session if you want to learn more about hybrid consultations, and discover the latest trends, tools and platforms enabling citizens to have their say in co-constructing the future of European democracy"
+      }
+    ]
+  },
+  {
+    "date": "07.10.20",
+    "time": "17:30 - 18:00",
+    "title": "The EuroPCom Evening Show",
+    "description": "Wind down with this humorous look back on Day 1 of EuroPCom."
+  }
+];
+
+const data8 = [
+  {
+    "date": "08.10.20",
+    "time": "08:45 - 09:00",
+    "title": "New day – new beginning",
+    "description": "Recap of Day 1 and intro to Day 2.",
+    "more": "View the outputs from the Ideas Labs the day before, and get ready for the second day of EuroPCom 2020 – Time for Communica[c]tion."
+  },
+  {
+    "date": "08.10.20",
+    "time": "09:00 - 12:15",
+    "title": "CIRCOM training",
+    "description": "More information",
+    "more": "How to reach and chase your audience online? What are the new storytelling formats? Join the session to get answers to these questions, talk with regional TV journalists and learn about the latest trends through case studies and practical exercises. This is a limited-availability training session, focused on local and regional journalists and media representatives. If you wish to attend this session, please select it when you register."
+  },
+  {
+    "date": "08.10.20",
+    "time": "09:00 - 17:00",
+    "title": "EuroPCom Market Place",
+    "description": "More information",
+    "more": "Our Virtual Marketplace will host multiple exhibitors who are eager to present their projects and exchange ideas with you. Please stroll around the virtual booths, make contact with the different organisations and projects, schedule 1:1 meetings, or follow group demo sessions. The Market Place will be open from lunchtime to 18:00 on day 1, and from 09:00 until 17:00 on day 2."
+  },
+  {
+    "date": "08.10.20",
+    "time": "09:00 - 10:30",
+    "title": "Workshops",
+    "type": "Workshops",
+    "description": "Follow the exchange between the panelists and seize the chance to fire your questions at them",
+    "more": "Second round of workshops on the three different themes, with topic experts who will share their ideas and experience with you. Please choose which workshop you wish to attend when you register.",
+    "workshops": [
+      {
+        "title": "For a truly inclusive debate",
+        "category": "Citizens",
+        "description": "",
+        "more": "The aspiration of the Conference on the Future of Europe is to give European citizens a leading and active role in the debate on the future of Europe. This is not new - EU institutions have been trying to connect better with citizens for decades. Could it be different this time? Why and how to involve citizens from the outset of the process? What alternative forms of active participation are out there? Can we learn from past experiences, failures and successes, to make this Conference a truly inclusive process? Come and discuss with panellists what works (and what does not) when it comes to citizens' engagement in decision-making."
+      },
+      {
+        "title": "From climate communication to behavioural change",
+        "category": "Green",
+        "more": "Informing about climate change and 'green' measures is important. But how do we actually make a difference with our communication? Our panel of experts will discuss how to foster behavioural change and stimulate the intended impact. Join this session to improve the effectiveness of your communication on climate action!"
+      },
+      {
+        "title": "Upcoming social media trends",
+        "category": "Digital",
+        "more": "Join us to discover what is trending and what to look out for in social media in the coming year."
+      }
+    ]
+  },
+  {
+    "date": "08.10.20",
+    "time": "10:30 - 10:45",
+    "title": "Digital Detox",
+    "description": "Time for a break – or have a quick visit to the EuroPCom Market Place!"
+  },
+  {
+    "date": "08.10.20",
+    "time": "10:45 - 12:15",
+    "type": "Ideas Labs",
+    "title": "Ideas Labs – time to experiment",
+    "description": "Deep dive into your selected theme to actively work online with other participants! Get prepared to share your ideas and experiences with the other members of the Lab!",
+    "more": '<p>The Ideas Labs will be very collaborative and participatory workshops, best suited for people who are willing to work together in small, facilitated groups to exchange their ideas and views with other participants. Each Ideas Lab will be different in terms of subject matter and what it expects to generate, from new ideas and concepts to concrete plans for future action.</p><p>Due to the participatory nature of the labs the number of participants in each Ideas Lab will be limited. Registrations for a particular Lab will be closed when sufficient people have indicated they wish to join it. Everyone who was able to register their interest will be allowed enter the "lobby" of their Ideas Lab shortly before it is due to begin, with access to the actual Lab granted on a first-come first-served basis.</p><p>If you do not wish to participate in an Ideas Lab, or the available spaces have all been taken up, we encourage you to visit the EuroPCom Market Place, meet other participants and find out more about the many projects and organisations who will be exhibiting.</p>',
+    "workshops": [
+      {
+        "title": "How regions and cities mobilise",
+        "category": "Citizens",
+        "more": "Many regions and cities across Europe are well ahead of the Conference on the Future of Europe when it comes to involving citizens in decision-making processes. Their potential to mobilise and engage citizens in public actions was clearly shown in the midst of the COVID-19 outbreak – they were on the front line to take initiatives to fight the pandemic. Regions and cities, as the closest and most trusted levels of government and proven actors of change, are well placed to make the link between the EU and its citizens. Many of them are now mobilising to join actively the debate in the Conference. Come and discover how regions and cities are mobilising for the future of EU democracy, and develop proposals on how to make this process more effective."
+      },
+      {
+        "title": "Learning from the legacy of miscommunicating climate science",
+        "category": "Green",
+        "more": "Climate change and global warming are unequivocal and many of the observed changes since the 1950s are unprecedented. But the details and findings can be very technical and lead to messages that are too complex or difficult and hence misunderstood. By looking back, can we find out together how we can improve climate communication for our future?"
+      },
+      {
+        "title": "Countering disinformation narratives",
+        "category": "Digital",
+        "more": "The COVID-19 outbreak led to not only a pandemic, but also an 'infodemic' of disinformation. Learn how to recognise and counter disinformation narratives through concrete case studies. Come and share your experience with identifying and preventing the circulation of fake news on social media and other communication channels."
+      }
+    ]
+  },
+  {
+    "date": "08.10.20",
+    "time": "12:15 - 12:30",
+    "title": "More Detox!",
+    "description": "Time for another break – or a quick visit to the EuroPCom Market Place!"
+  },
+  {
+    "date": "08.10.20",
+    "time": "12:30 - 13:30",
+    "title": "Closing session – Ask your questions!",
+    "description": "Challenge the EU communication VIPs",
+    "more": "Interactive session with the Heads of Communication of the various EU institutions involved in EuroPCom. Are there any remaining questions after two days of EuroPCom? The closing session is the occasion to ask them! Our speakers discuss and close this year's edition of EuroPCom with all the answers you need to go from communication to action!"
+  },
+  {
+    "date": "08.10.20",
+    "time": "13:30 - 14:15",
+    "title": "Aftershow - Social media after-party  ",
+    "description": "Sip some virtual cocktails (or mocktails if you need to keep working!) and celebrate the end of the 11th edition of EuroPCom!"
+  }
+];
+
+
+module.exports = {
+  preview: "@europcompreview",
+  context: {
+    program7th: data7,
+    program8th: data8
+  }
+};
--- components/europcom/speakers.config.js
+++ components/europcom/speakers.config.js
@@ -6,7 +6,7 @@ const speakersData = [];
 for (var i = 0; i < speakersCount; i++) {
   speakersData.push({
     name: faker.name.findName(),
-    picture: "https://fakeface.rest/face/view/" + i + "?minimum_age=25",
+    picture: `https://fakeface.rest/face/view/${i}?minimum_age=25`,
     jobTitle: faker.name.jobTitle(),
     company: faker.company.companyName(),
     bio: faker.lorem.paragraphs(),
