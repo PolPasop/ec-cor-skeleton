2bdf475a494037ce7ca2419ebd72194de01a57cc
Jesse Govart <govartjessy@gmail.com>
2020-11-16 07:46:11 +0100

    program

--- public/js/europcom.js
+++ public/js/europcom.js
@@ -4,13 +4,25 @@
   const detailedButton = document.querySelector("#detailedAgenda");
   const summaryButton = document.querySelector("#summarydAgenda");
 
+  const activeButtons = () => document.querySelectorAll(".ec-europcom-program .active");
+
   const getExpandedElements = () => document.querySelectorAll("details");
+
+  const toggleClass = el => {
+    [...activeButtons()].map(element => element.classList.remove("active"));
+    el.classList.add("active");
+  };
 
   const changeVisibility = boolean => {
     [...getExpandedElements()].map(element => element.open = boolean);
   };
 
-  detailedButton.addEventListener("click", () => changeVisibility(true));
-  summaryButton.addEventListener("click", () => changeVisibility(false));
+  const onClick = (el, text) => {
+    toggleClass(el);
+    text === "detailedView" ? changeVisibility(true) : changeVisibility(false);
+  };
+
+  detailedButton.addEventListener("click", () => onClick(event.target, "detailedView"));
+  summaryButton.addEventListener("click", () => onClick(event.target, "summaryView"));
 
 }());
