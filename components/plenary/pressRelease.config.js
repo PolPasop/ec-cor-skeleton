a3f7d036168aabb67393b699fea62abb1545b239
Jesse Govart <govartjessy@gmail.com>
2020-12-01 16:15:08 +0100

    maincontent in preview

---
+++ components/plenary/pressRelease.config.js
@@ -0,0 +1,21 @@
+const faker = require('faker');
+
+const pressReleasesCount = 32;
+const pressReleasesData = [];
+
+for (var i = 0; i < pressReleasesCount; i++) {
+  pressReleasesData.push({
+    picture: `https://fakeface.rest/face/view/${i}?minimum_age=25`,
+    title: faker.lorem.sentence(),
+    name: faker.name.findName(),
+    country: faker.address.country(),
+    group: faker.company.companyName(),
+    role: "Rapporteur"
+  });
+}
+
+module.exports = {
+  context: {
+    pressReleases: pressReleasesData
+  }
+};
